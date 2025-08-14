5d478ded707968e1c02fcdb9662d94bed48205e4
taka <jj4068851@gmail.com>
2025-08-14 04:01:25 +0000

    feat: task loop count

--- main/zenithFinance/usdcLiquidity.ts
+++ main/zenithFinance/usdcLiquidity.ts
@@ -8,38 +8,43 @@ import { sleep } from "@scripts/utils/time"
 const router = "0xf8a1d4ff0f9b9af7ce58e1fc1833688f3bfd6115"
 export default async function usdcLiquidityZenith() {
      const env = envLoaded()
-     const deadline = Math.floor(Date.now() / 1000) + 60 * 10
-     const poolAddressUsdcUsdt = pharosPoolAddressZenith.filter(pool => pool.pair == "USDC/USDT")[0].address
-     const usdcAddress = pharosTokenAddress.filter(item => item.name == "USDC")[0].address
-     const usdtAddress = pharosTokenAddress.filter(item => item.name == "USDT")[0].address
-
-     await liquidity({
-          signer: wallet.signer,
-          poolAddress: poolAddressUsdcUsdt,
-          tokenA: usdcAddress,
-          tokenB: usdtAddress,
-          provider,
-          amountInPercent: env.AMOUNT_IN_PERCENT,
-          router,
-          deadline
-     })
-     const ms = randomAmount({
-          min: env.TIMEOUT_MIN_MS,
-          max: env.TIMEOUT_MAX_MS
-     })
-     await sleep(ms)
+     for (let index = 1; index <= env.LOOP_COUNT; index++){
+          console.log(`Task lp usdc ${index}/${env.LOOP_COUNT}`)
+          const deadline = Math.floor(Date.now() / 1000) + 60 * 10
+          const poolAddressUsdcUsdt = pharosPoolAddressZenith.filter(pool => pool.pair == "USDC/USDT")[0].address
+          const usdcAddress = pharosTokenAddress.filter(item => item.name == "USDC")[0].address
+          const usdtAddress = pharosTokenAddress.filter(item => item.name == "USDT")[0].address
      
-     const poolAddressUsdcWphrs = pharosPoolAddressZenith.filter(pool => pool.pair == "USDC/PHRS")[0].address
-     const wphrsAddress = pharosTokenAddress.filter(item => item.name == "WPHRS")[0].address
-
-     await liquidity({
-          signer: wallet.signer,
-          poolAddress: poolAddressUsdcWphrs,
-          tokenA: usdcAddress,
-          tokenB: wphrsAddress,
-          provider,
-          amountInPercent: env.AMOUNT_IN_PERCENT,
-          router,
-          deadline
-     })
+          await liquidity({
+               signer: wallet.signer,
+               poolAddress: poolAddressUsdcUsdt,
+               tokenA: usdcAddress,
+               tokenB: usdtAddress,
+               provider,
+               amountInPercent: env.AMOUNT_IN_PERCENT,
+               router,
+               deadline
+          })
+          const ms = randomAmount({
+               min: env.TIMEOUT_MIN_MS,
+               max: env.TIMEOUT_MAX_MS
+          })
+          await sleep(ms)
+          
+          const poolAddressUsdcWphrs = pharosPoolAddressZenith.filter(pool => pool.pair == "USDC/PHRS")[0].address
+          const wphrsAddress = pharosTokenAddress.filter(item => item.name == "WPHRS")[0].address
+     
+          await liquidity({
+               signer: wallet.signer,
+               poolAddress: poolAddressUsdcWphrs,
+               tokenA: usdcAddress,
+               tokenB: wphrsAddress,
+               provider,
+               amountInPercent: env.AMOUNT_IN_PERCENT,
+               router,
+               deadline
+          })
+     }
 }
+
+usdcLiquidityZenith().catch(console.error)
--- main/zenithFinance/wphrsSwap.ts
+++ main/zenithFinance/wphrsSwap.ts
@@ -9,106 +9,105 @@ import { randomAmount } from "@scripts/utils/amount"
 const router = "0x1a4de519154ae51200b0ad7c90f7fac75547888a"
 export default async function wphrsSwapZenith() {
      const env = envLoaded()
-     const deadline = Math.floor(Date.now() / 1000) + 60 * 10
-     const poolAddressWphrsUsdc = pharosPoolAddressZenith.filter(pool => pool.pair == "USDC/PHRS")[0].address
-     const usdcAddress = pharosTokenAddress.filter(item => item.name == "USDC")[0].address
-     const wphrsAddress = pharosTokenAddress.filter(item => item.name == "WPHRS")[0].address
-     
+     for (let index = 1; index <= env.LOOP_COUNT; index++) {
+          console.log(`Task swap wphrs ${index}/${env.LOOP_COUNT}`)
+          const deadline = Math.floor(Date.now() / 1000) + 60 * 10
+          const poolAddressWphrsUsdc = pharosPoolAddressZenith.filter(pool => pool.pair == "USDC/PHRS")[0].address
+          const usdcAddress = pharosTokenAddress.filter(item => item.name == "USDC")[0].address
+          const wphrsAddress = pharosTokenAddress.filter(item => item.name == "WPHRS")[0].address
 
-     const priceWphrsUsdc = await getPrice({
-          poolAddress: poolAddressWphrsUsdc,
-          addresses: {
-               tokenA: wphrsAddress,
-               tokenB: usdcAddress
-          },
-          provider
-     })
-     let {balance: wphrsBalance, symbol: wphrsSymbol, decimals: wphrsDecimals} = await tokenBalance({
-          address: wallet.address,
-          provider,
-          tokenAddress: wphrsAddress
-     })
 
-     const {symbol: usdcSymbol, decimals: usdcDecimals} = await tokenBalance({
-          address: wallet.address,
-          provider,
-          tokenAddress: usdcAddress
-     })
-
-     const slippageTolerance = 0.003 // 0.3%
-
-     // Convert float price (like 0.1484) into scaled integer
-     let priceScaled = BigInt(Math.floor(priceWphrsUsdc.tokenAToTokenB * Number(10n ** usdcDecimals)))
-
-     // amountIn: 10% of WPHRS balance
-     let amountIn = wphrsBalance * BigInt(env.AMOUNT_IN_PERCENT) / 100n
-
-     // amountOut: (amountIn * priceScaled * (1 - slippage)) / DECIMALS_WPHRS
-     let amountOut = (amountIn * priceScaled * BigInt(1000 - Math.floor(slippageTolerance * 1000))) / ((10n ** wphrsDecimals) * 1000n)
-
-     console.log(`SWAP ${wphrsSymbol}/${usdcSymbol}`)
-     try {
-          console.log("Swaping...")
-          await multicall({
-               router,
-               tokenIn: wphrsAddress,
-               tokenOut: usdcAddress,
-               amountIn,
-               amountOut,
-               fee: priceWphrsUsdc.fee,
-               signer: wallet.signer,
-               deadline
+          const priceWphrsUsdc = await getPrice({
+               poolAddress: poolAddressWphrsUsdc,
+               addresses: {
+                    tokenA: wphrsAddress,
+                    tokenB: usdcAddress
+               },
+               provider
           })
-     } catch (error) {
-          console.error(`Swap failed: ${error}`)
-     }
-
-     const ms = randomAmount({
-          min: env.TIMEOUT_MIN_MS,
-          max: env.TIMEOUT_MAX_MS
-     })
-     await sleep(ms)
-
-     const poolAddressWphrsUsdt = pharosPoolAddressZenith.filter(pool => pool.pair == "USDT/PHRS")[0].address
-     const usdtAddress = pharosTokenAddress.filter(item => item.name == "USDT")[0].address
-
-     const priceWphrsUsdt = await getPrice({
-          poolAddress: poolAddressWphrsUsdt,
-          addresses: {
-               tokenA: wphrsAddress,
-               tokenB: usdtAddress
-          },
-          provider
-     });
-
-     ({balance: wphrsBalance} = await tokenBalance({
-          address: wallet.address,
-          provider,
-          tokenAddress: wphrsAddress
-     }))
-     let {symbol: usdtSymbol, decimals: usdtDecimals} = await tokenBalance({
-          address: wallet.address,
-          provider,
-          tokenAddress: usdtAddress
-     })
-     priceScaled = BigInt(Math.floor(priceWphrsUsdt.tokenAToTokenB * Number(10n ** usdtDecimals)))
-     amountIn = wphrsBalance * BigInt(env.AMOUNT_IN_PERCENT) / 100n
-     amountOut = amountOut = (amountIn * priceScaled * BigInt(1000 - Math.floor(slippageTolerance * 1000))) / ((10n ** wphrsDecimals) * 1000n)
-     console.log(`SWAP ${wphrsSymbol}/${usdtSymbol}`)
-     try {
-          console.log("Swaping...")
-          await multicall({
-               router,
-               tokenIn: wphrsAddress,
-               tokenOut: usdtAddress,
-               amountIn,
-               amountOut,
-               fee: priceWphrsUsdt.fee,
-               signer: wallet.signer,
-               deadline
+          let { balance: wphrsBalance, symbol: wphrsSymbol, decimals: wphrsDecimals } = await tokenBalance({
+               address: wallet.address,
+               provider,
+               tokenAddress: wphrsAddress
           })
-     } catch (error) {
-          console.error(`Swap failed: ${error}`)
+
+          const { symbol: usdcSymbol, decimals: usdcDecimals } = await tokenBalance({
+               address: wallet.address,
+               provider,
+               tokenAddress: usdcAddress
+          })
+
+          const slippageTolerance = 0.003
+          let priceScaled = BigInt(Math.floor(priceWphrsUsdc.tokenAToTokenB * Number(10n ** usdcDecimals)))
+          let amountIn = wphrsBalance * BigInt(env.AMOUNT_IN_PERCENT) / 100n
+          let amountOut = (amountIn * priceScaled * BigInt(1000 - Math.floor(slippageTolerance * 1000))) / ((10n ** wphrsDecimals) * 1000n)
+
+          console.log(`SWAP ${wphrsSymbol}/${usdcSymbol}`)
+          try {
+               console.log("Swaping...")
+               await multicall({
+                    router,
+                    tokenIn: wphrsAddress,
+                    tokenOut: usdcAddress,
+                    amountIn,
+                    amountOut,
+                    fee: priceWphrsUsdc.fee,
+                    signer: wallet.signer,
+                    deadline
+               })
+          } catch (error) {
+               console.error(`Swap failed: ${error}`)
+          }
+
+          const ms = randomAmount({
+               min: env.TIMEOUT_MIN_MS,
+               max: env.TIMEOUT_MAX_MS
+          })
+          await sleep(ms)
+
+          const poolAddressWphrsUsdt = pharosPoolAddressZenith.filter(pool => pool.pair == "USDT/PHRS")[0].address
+          const usdtAddress = pharosTokenAddress.filter(item => item.name == "USDT")[0].address
+
+          const priceWphrsUsdt = await getPrice({
+               poolAddress: poolAddressWphrsUsdt,
+               addresses: {
+                    tokenA: wphrsAddress,
+                    tokenB: usdtAddress
+               },
+               provider
+          });
+
+          ({ balance: wphrsBalance } = await tokenBalance({
+               address: wallet.address,
+               provider,
+               tokenAddress: wphrsAddress
+          }))
+          let { symbol: usdtSymbol, decimals: usdtDecimals } = await tokenBalance({
+               address: wallet.address,
+               provider,
+               tokenAddress: usdtAddress
+          })
+          priceScaled = BigInt(Math.floor(priceWphrsUsdt.tokenAToTokenB * Number(10n ** usdtDecimals)))
+          amountIn = wphrsBalance * BigInt(env.AMOUNT_IN_PERCENT) / 100n
+          amountOut = amountOut = (amountIn * priceScaled * BigInt(1000 - Math.floor(slippageTolerance * 1000))) / ((10n ** wphrsDecimals) * 1000n)
+          console.log(`SWAP ${wphrsSymbol}/${usdtSymbol}`)
+          try {
+               console.log("Swaping...")
+               await multicall({
+                    router,
+                    tokenIn: wphrsAddress,
+                    tokenOut: usdtAddress,
+                    amountIn,
+                    amountOut,
+                    fee: priceWphrsUsdt.fee,
+                    signer: wallet.signer,
+                    deadline
+               })
+          } catch (error) {
+               console.error(`Swap failed: ${error}`)
+          }
      }
 
 }
+
+wphrsSwapZenith().catch(console.error)
