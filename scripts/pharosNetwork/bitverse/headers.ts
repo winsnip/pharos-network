c208e14d22a9957d48f12200bd5c225579a7b9b7
taka <jj4068851@gmail.com>
2025-10-20 00:16:14 +0000

    fix: update selector, params and undetected usdt balance

--- scripts/pharosNetwork/bitverse/headers.ts
+++ scripts/pharosNetwork/bitverse/headers.ts
@@ -2,5 +2,6 @@ export const headers = {
      "Origin": "https://testnet.bitverse.zone",
      "Referer": "https://testnet.bitverse.zone",
      "Accept": "*/*",
-     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+     "tenant-id": "PHAROS"
 }
